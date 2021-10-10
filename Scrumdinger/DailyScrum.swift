077f377832d6d78902d857ed768207dfbecbde8d
William Ahern <willdahern@gmail.com>
2021-10-10 15:49:34 -0400

    Add Scrumdinger views for daily scrums

---
+++ Scrumdinger/DailyScrum.swift
@@ -0,0 +1,36 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by William Ahern on 10/7/21.
+//
+
+import SwiftUI
+
+struct DailyScrum: Identifiable {
+    let id: UUID
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var color: Color
+    
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, color: Color) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.color = color
+    }
+}
+
+extension DailyScrum {
+    static var data: [DailyScrum] {
+        [
+            DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon"], lengthInMinutes: 10, color: Color("Design")),
+            DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, color: Color("App Dev")),
+            DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color("Web Dev"))
+        ]
+    }
+}
+
+ 
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by William Ahern on 10/8/21.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
